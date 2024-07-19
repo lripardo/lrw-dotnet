3186adcf9437842091a140a1a11d8bbfbfce5fb5
Luiz Ricardo Ripardo <ricardo_ripardo@hotmail.com>
2024-07-19 13:32:17 -0300

    refactor: dictionary config source

--- LRW.Core/Configuration/DictionaryConfigSource.cs
+++ LRW.Core/Configuration/DictionaryConfigSource.cs
@@ -2,10 +2,8 @@
 
 public class DictionaryConfigSource(Dictionary<string, string> config) : IConfigSource
 {
-    public Dictionary<string, string> Config { get; set; } = config;
-
     public string Get(string key)
     {
-        return Config[key];
+        return config[key];
     }
 }
