61ffabe046a0809366beedcba0e11afef11edb37
Luiz Ricardo Ripardo <ricardo_ripardo@hotmail.com>
2024-07-19 16:20:08 -0300

    refactor: response moved to communication packages

---
+++ LRW.Core/Communication/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace LRW.Core.Communication;
+
+public abstract class Response(string id, int status, object? data)
+{
+    public string Id { get; } = id;
+    public int Status { get; } = status;
+    public object? Data { get; } = data;
+}
