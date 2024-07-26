f9607011f80d37b814d3bb61ddbb915ce732f25f
Luiz Ricardo Ripardo <ricardo_ripardo@hotmail.com>
2024-07-26 20:43:51 -0300

    tests: cache memory complementary

--- LRW.UnitTests/Core/CacheTests.cs
+++ LRW.UnitTests/Core/CacheTests.cs
@@ -6,74 +6,56 @@ using System.Reflection;
 
 namespace LRW.UnitTests.Core;
 
+public static class CacheTestsExtensions
+{
+    public static ConcurrentDictionary<string, MemoryCacheItem<string>> GetDictionary(this MemoryCache<string> cache)
+    {
+        var dictionary = typeof(MemoryCache<string>).GetField("_dictionary", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        return (ConcurrentDictionary<string, MemoryCacheItem<string>>)dictionary?.GetValue(cache)!;
+    }
+}
+
 public class CacheTests
 {
     #region MemoryCache
 
     [Fact]
-    public void Set_MustStoreCorrectObjectWithCorrectExpiresAt()
+    public void Set_MustStoreCorrectObject()
     {
         //Arrange
-        var dictionary = new ConcurrentDictionary<string, MemoryCacheItem<string>>();
-
-        var dateResolver = A.Fake<IDateTimeResolver>();
-        var cache = new MemoryCache<string>(dateResolver);
-
-        A.CallTo(() => dateResolver.Now).Returns(DateTime.Now);
+        var cache = new MemoryCache<string>(A.Fake<IDateTimeResolver>());
 
         //Act
-        cache.Set("TEST1", "MY VALUE", TimeSpan.Zero);
-        
-        typeof(MemoryCache<string>).GetField("_dictionary", BindingFlags.NonPublic | BindingFlags.Instance)?.SetValue(cache, dictionary);
+        cache.Set("TEST1", "MY VALUE 1", TimeSpan.Zero);
 
         //Assert
-        Assert.NotNull(dictionary);
+        var result = cache.GetDictionary()["TEST1"];
 
-        var result = dictionary["TEST1"];
-
-        Assert.Equal("MY VALUE", result.Value);
-        Assert.Null(result.ExpiresAt);
-
-        A.CallTo(() => dateResolver.Now).MustNotHaveHappened();
+        Assert.Equal("MY VALUE 1", result.Value);
     }
 
     [Fact]
-    public async Task SetAsync_MustStoreCorrectObjectWithCorrectExpiresAt()
+    public async Task SetAsync_MustStoreCorrectObject()
     {
         //Arrange
-        var dictionary = new ConcurrentDictionary<string, MemoryCacheItem<string>>();
-
-        var dateResolver = A.Fake<IDateTimeResolver>();
-        var cache = new MemoryCache<string>(dateResolver);
-
-        A.CallTo(() => dateResolver.Now).Returns(DateTime.Now);
+        var cache = new MemoryCache<string>(A.Fake<IDateTimeResolver>());
 
         //Act
-        await cache.SetAsync("TEST1", "MY VALUE", TimeSpan.Zero);
-
-        typeof(MemoryCache<string>).GetField("_dictionary", BindingFlags.NonPublic | BindingFlags.Instance)?.SetValue(cache, dictionary);
+        await cache.SetAsync("TEST1", "MY VALUE 1", TimeSpan.Zero);
 
         //Assert
-        Assert.NotNull(dictionary);
+        var result = cache.GetDictionary()["TEST1"];
 
-        var result = dictionary["TEST1"];
-
-        Assert.Equal("MY VALUE", result.Value);
-        Assert.Null(result.ExpiresAt);
-
-        A.CallTo(() => dateResolver.Now).MustNotHaveHappened();
+        Assert.Equal("MY VALUE 1", result.Value);
     }
 
     [Fact]
     public void Get_MustReturnsStoredObject()
     {
         //Arrange
-        var dictionary = new ConcurrentDictionary<string, MemoryCacheItem<string>> { ["TEST2"] = new("MY VALUE 2") };
-
-        var dateResolver = A.Fake<IDateTimeResolver>();
-        var cache = new MemoryCache<string>(dateResolver);
-
-        typeof(MemoryCache<string>).GetField("_dictionary", BindingFlags.NonPublic | BindingFlags.Instance)?.SetValue(cache, dictionary);
+        var cache = new MemoryCache<string>(A.Fake<IDateTimeResolver>());
+        cache.GetDictionary()["TEST2"] = new MemoryCacheItem<string>("MY VALUE 2");
 
         //Act
         var result = cache.Get("TEST2");
@@ -81,17 +63,13 @@ public class CacheTests
         //Assert
         Assert.Equal("MY VALUE 2", result);
     }
-    
+
     [Fact]
     public async Task GetAsync_MustReturnsStoredObject()
     {
         //Arrange
-        var dictionary = new ConcurrentDictionary<string, MemoryCacheItem<string>> { ["TEST2"] = new("MY VALUE 2") };
-
-        var dateResolver = A.Fake<IDateTimeResolver>();
-        var cache = new MemoryCache<string>(dateResolver);
-
-        typeof(MemoryCache<string>).GetField("_dictionary", BindingFlags.NonPublic | BindingFlags.Instance)?.SetValue(cache, dictionary);
+        var cache = new MemoryCache<string>(A.Fake<IDateTimeResolver>());
+        cache.GetDictionary()["TEST2"] = new MemoryCacheItem<string>("MY VALUE 2");
 
         //Act
         var result = await cache.GetAsync("TEST2");
@@ -105,12 +83,10 @@ public class CacheTests
     public void Del_MustRemoveObjectFromDictionary()
     {
         //Arrange
-        var dictionary = new ConcurrentDictionary<string, MemoryCacheItem<string>> { ["TEST3"] = new("MY VALUE 3") };
+        var cache = new MemoryCache<string>(A.Fake<IDateTimeResolver>());
+        var dictionary = cache.GetDictionary();
 
-        var dateResolver = A.Fake<IDateTimeResolver>();
-        var cache = new MemoryCache<string>(dateResolver);
-
-        typeof(MemoryCache<string>).GetField("_dictionary", BindingFlags.NonPublic | BindingFlags.Instance)?.SetValue(cache, dictionary);
+        dictionary["TEST3"] = new MemoryCacheItem<string>("MY VALUE 3");
 
         //Act
         cache.Del("TEST3");
@@ -123,18 +99,47 @@ public class CacheTests
     public async Task DelAsync_MustRemoveObjectFromDictionary()
     {
         //Arrange
-        var dictionary = new ConcurrentDictionary<string, MemoryCacheItem<string>> { ["TEST3"] = new("MY VALUE 3") };
+        var cache = new MemoryCache<string>(A.Fake<IDateTimeResolver>());
+        var dictionary = cache.GetDictionary();
 
-        var dateResolver = A.Fake<IDateTimeResolver>();
-        var cache = new MemoryCache<string>(dateResolver);
-
-        typeof(MemoryCache<string>).GetField("_dictionary", BindingFlags.NonPublic | BindingFlags.Instance)?.SetValue(cache, dictionary);
+        dictionary["TEST3"] = new MemoryCacheItem<string>("MY VALUE 3");
 
         //Act
         await cache.DelAsync("TEST3");
 
         //Assert
         Assert.Throws<KeyNotFoundException>(() => dictionary["TEST3"]);
+    }
+
+    [Fact]
+    public void ObjectMustBeStoredWithInfiniteTimeWhenTimestampIsZero()
+    {
+        //Arrange
+        var cache = new MemoryCache<string>(A.Fake<IDateTimeResolver>());
+
+        //Act
+        cache.Set("TEST4", "MY VALUE 4", TimeSpan.Zero);
+
+        //Assert
+        Assert.Equal("MY VALUE 4", cache.Get("TEST4"));
+    }
+
+    [Fact]
+    public void ObjectExpiredMustBeDeletedFromDictionary()
+    {
+        //Arrange
+        var dateResolver = A.Fake<IDateTimeResolver>();
+        var cache = new MemoryCache<string>(dateResolver);
+        cache.GetDictionary()["TEST5"] = new MemoryCacheItem<string>("MY VALUE 5", new DateTime(999));
+
+        A.CallTo(() => dateResolver.Now).Returns(new DateTime(1000));
+
+        //Act
+        var result = cache.Get("TEST5");
+
+        //Assert
+        Assert.Null(result);
+        Assert.Throws<KeyNotFoundException>(() => cache.GetDictionary()["TEST5"]);
     }
 
     #endregion
