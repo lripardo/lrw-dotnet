e3e986740ce997172ce207964573eb13b775ecd5
Luiz Ricardo Ripardo <ricardo_ripardo@hotmail.com>
2024-09-04 12:40:07 -0300

    test: added invalid responses to hcaptcha

--- LRW.IntegrationTests/Core/CaptchaTests.cs
+++ LRW.IntegrationTests/Core/CaptchaTests.cs
@@ -1,4 +1,5 @@
-﻿using LRW.Core.Captcha;
+﻿using FluentValidation;
+using LRW.Core.Captcha;
 using LRW.Core.Configuration;
 using LRW.Core.Date;
 using Xunit.Abstractions;
@@ -7,19 +8,32 @@ namespace LRW.IntegrationTests.Core;
 
 public class CaptchaTests(ITestOutputHelper output)
 {
+    private const string ValidRemoteIp = "127.0.0.1";
+    private const string ValidResponse = "10000000-aaaa-bbbb-cccc-000000000001";
+    private const string ValidSiteKey = "10000000-ffff-ffff-ffff-000000000001";
+    private const string ValidSecret = "0x0000000000000000000000000000000000000000";
+
     [Fact]
     public async Task HCaptcha_Validate_MustNotThrowExceptionWhenTokenIsValid()
     {
         //Arrange
-        var src = new DictionaryConfigSource() { { "HCAPTCHA_SECRET", "0x0000000000000000000000000000000000000000" } };
+        var src = new DictionaryConfigSource() { { "HCAPTCHA_SECRET", ValidSecret } };
         var hcaptcha = new HCaptchaValidator(new KeyedConfig(src), new XUnitLogger(output), new SystemDateTimeResolver());
 
-        //Act
-        await hcaptcha.Validate(new HCaptchaInput()
-        {
-            RemoteIp = "127.0.0.1",
-            Response = "10000000-aaaa-bbbb-cccc-000000000001",
-            SiteKey = "10000000-ffff-ffff-ffff-000000000001",
-        });
+        //Act and Assert
+        await hcaptcha.Validate(new HCaptchaInput() { RemoteIp = ValidRemoteIp, Response = ValidResponse, SiteKey = ValidSiteKey });
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("any invalid key")]
+    public async Task HCaptcha_Validate_MustThrowValidationExceptionOnInvalidResponses(string response)
+    {
+        //Arrange
+        var src = new DictionaryConfigSource() { { "HCAPTCHA_SECRET", ValidSecret } };
+        var hcaptcha = new HCaptchaValidator(new KeyedConfig(src), new XUnitLogger(output), new SystemDateTimeResolver());
+
+        //Act and Assert
+        await Assert.ThrowsAsync<ValidationException>(() => hcaptcha.Validate(new HCaptchaInput() { Response = response, SiteKey = ValidSiteKey, RemoteIp = ValidRemoteIp }));
     }
 }
