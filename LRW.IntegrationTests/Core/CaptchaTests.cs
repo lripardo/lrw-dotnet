266ccbf73db4afc30723564001fc7ecbe080aaa9
Luiz Ricardo Ripardo <ricardo_ripardo@hotmail.com>
2024-09-04 12:14:14 -0300

    test: captcha single test with logger

---
+++ LRW.IntegrationTests/Core/CaptchaTests.cs
@@ -0,0 +1,25 @@
+﻿using LRW.Core.Captcha;
+using LRW.Core.Configuration;
+using LRW.Core.Date;
+using Xunit.Abstractions;
+
+namespace LRW.IntegrationTests.Core;
+
+public class CaptchaTests(ITestOutputHelper output)
+{
+    [Fact]
+    public async Task HCaptcha_Validate_MustNotThrowExceptionWhenTokenIsValid()
+    {
+        //Arrange
+        var src = new DictionaryConfigSource() { { "HCAPTCHA_SECRET", "0x0000000000000000000000000000000000000000" } };
+        var hcaptcha = new HCaptchaValidator(new KeyedConfig(src), new XUnitLogger(output), new SystemDateTimeResolver());
+
+        //Act
+        await hcaptcha.Validate(new HCaptchaInput()
+        {
+            RemoteIp = "127.0.0.1",
+            Response = "10000000-aaaa-bbbb-cccc-000000000001",
+            SiteKey = "10000000-ffff-ffff-ffff-000000000001",
+        });
+    }
+}
---
+++ LRW.IntegrationTests/XUnitLogger.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace LRW.IntegrationTests;
+
+public class XUnitLogger(ITestOutputHelper helper) : ILogger
+{
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        helper.WriteLine($"[{logLevel}] - {formatter(state, exception)}");
+    }
+
+    public bool IsEnabled(LogLevel logLevel) => true;
+
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull => null;
+}
